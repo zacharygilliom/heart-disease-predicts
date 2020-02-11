34f5e11c0d0d426916a76a2bef3eb3afda5a960c
zacharygilliom <zacharygilliom@gmail.com>
2020-02-11 09:45:56 -0500

    Created our classifier model, updated the readme, added documentation, and added some images.

--- README.md
+++ README.md
@@ -1,2 +1,29 @@
 # heart-disease-predicts
 Lets try to predict whether a patient has heart disase
+
+Started out by plotting some of the variables in our data to get a look at the distribution.
+
+![variable_distributions](https://user-images.githubusercontent.com/23482152/74297928-504c0500-4d16-11ea-9b9d-8b454591e0a9.png)
+
+Along with the distribution we want to see if there are any missing values in our data.
+
+![variable_distributions](https://user-images.githubusercontent.com/23482152/74297928-504c0500-4d16-11ea-9b9d-8b454591e0a9.png)
+
+Luckily we have a clean dataset that is ready for analysis.
+
+Decided to go with a Logistic Regression.  Due to the small size of our dataset this seems to be the most efficient and best option.
+
+Once we run our model we want to check out how it scores on both our train subset and our test subset.
+
+![Model_scores](https://user-images.githubusercontent.com/23482152/74298204-0879ad80-4d17-11ea-86da-f23f95f9c483.png)
+
+Once we have our scores, it could always be better, but in the meantime prior to making our model better we can check out some of our metrics.
+
+
+![classification_report](https://user-images.githubusercontent.com/23482152/74298282-3fe85a00-4d17-11ea-99a1-dafa03785231.png)
+
+Lastly we have our matrix that shows how our predicted values compares to our actual values.
+
+![actual_vs_predicted](https://user-images.githubusercontent.com/23482152/74298318-5abace80-4d17-11ea-814f-1f4223e729e7.png)
+
+This was a fun little machine learning project.  Some ways to expand on the project would be to use some other classification techniques.  But our data is pretty limited, so our options for which classifier to use is limited also.
--- main.py
+++ main.py
@@ -1,9 +1,13 @@
 import pandas as pd 
 import matplotlib.pyplot as plt 
 import seaborn as sns
+import numpy as np 
 
 from sklearn.ensemble import RandomForestRegressor
 from sklearn.model_selection import *
+from sklearn.linear_model import LogisticRegression
+from sklearn.metrics import *
+from sklearn.preprocessing import StandardScaler
 
 
 def exploratory_summary(df):
@@ -19,26 +23,23 @@ def exploratory_summary(df):
 	print(df.describe())
 	return None
 
+# Want to plot the distribtution of our continuous variables.  
 def plot(df):
 	a = list(range(1, len(get_cols(df)) + 1))
-	print(a)
 	b = get_cols(df)
-	print(b)
 	c = len(get_cols(df))
-	print(c)
 	figure, axes = plt.subplots(nrows=c, ncols=1)
 	figure.tight_layout(pad=1.05)
+	colors = ['b', 'r', 'g', 'c', 'm']
+	k = 0
 	for i, j in zip(a, b):
 		print(i, j)
 		plt.subplot(c,1,i)
-		sns.distplot(df[j])
+		sns.distplot(df[j], color=colors[k])
+		k += 1
 	plt.show()
 
-def slice_data(df):
-	df = df[['age', 'trestbps', 'chol', 'fbs', 'restecg',
-		'thalach', 'ca']]
-	return df
-
+# this function will grab all of the columns that are continuous.
 def get_cols(df):
 	cols = []
 	for col in df.columns:
@@ -49,16 +50,69 @@ def get_cols(df):
 
 df = pd.read_csv('heart.csv')
 
-
-sliced_data = slice_data(df)
-# The column "Unnamed: 0" has no value as it is just a repeat of our index
-
-# Lets get an idea of how our data is shaped
+# Lets get an idea of how our data is shaped.  Looks like our data
+# is organized and not missing any values.
 exploratory_summary(df)
-# exploratory_summary(sliced_data)
-# Lets do some plotting of our variables to see how they are related
 
+# Lets do some plotting of our variables to see how they are related.
 plot(df)
 
-# sns.pairplot(sliced_data)
-# plt.show()
+# Selecting all of our variables as our features.
+features = ['age', 'sex', 'cp', 'trestbps', 'chol', 'fbs', 'restecg',
+			'thalach', 'exang', 'oldpeak', 'slope', 'ca', 'thal']
+X = df[features]
+
+
+outcome = ['target']
+y = df[outcome]
+
+# We want our target variable to be a flattened array.
+y = np.ravel(y)
+
+# Since we only have 1 dataset, we will need to split it.
+X_train, X_test, y_train, y_test = train_test_split(X,y, train_size=.8)
+
+
+scaler = StandardScaler()
+X_train = scaler.fit_transform(X_train)
+
+scaler = StandardScaler()
+X_test = scaler.fit_transform(X_test)
+
+# Initialize our model.  Chose LibLinear as this is the suggested model for 
+# smaller datasets.
+log_model = LogisticRegression(solver='liblinear', random_state=1,
+								C=10)
+
+log_model.fit(X_train, y_train)
+
+# want to run some of our metrics on our model
+print('*** probability prediction ***')
+print(log_model.predict_proba(X_train))
+
+print('** Model Score on Train Data ***')
+print(log_model.score(X_train, y_train))
+
+print('*** Model Score on Test Data ***')
+print(log_model.score(X_test, y_test))
+
+print('*** The Confusion Matrix ***')
+cm = confusion_matrix(y_test, log_model.predict(X_test))
+print(cm)
+
+print('Classification Report')
+print(classification_report(y_test, log_model.predict(X_test)))
+
+
+# we can plot our confusion matrix to visualize the expected 
+# outcomes vs the actual outcomes
+fig, ax = plt.subplots(figsize=(8, 8))
+ax.imshow(cm)
+ax.grid(False)
+ax.xaxis.set(ticks=(0, 1), ticklabels=('Predicted 0s', 'Predicted 1s'))
+ax.yaxis.set(ticks=(0, 1), ticklabels=('Actual 0s', 'Actual 1s'))
+ax.set_ylim(1.5, -0.5)
+for i in range(2):
+    for j in range(2):
+        ax.text(j, i, cm[i, j], ha='center', va='center', color='red')
+plt.show()
